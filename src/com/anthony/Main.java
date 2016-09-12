45c8a24d27303670a89bce72148ce13e39a9a717
Anthony <Home>
2016-09-12 16:39:53 -0500

    Complete

--- src/com/anthony/Main.java
+++ src/com/anthony/Main.java
@@ -1,8 +1,32 @@
 package com.anthony;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    static Scanner numberScanner = new Scanner(System.in);
+    static Scanner stringScanner = new Scanner(System.in);
     public static void main(String[] args) {
-	// write your code here
+        // Get distance traveled
+        System.out.println("What distance did you travel? ");
+        // Create variables
+        double distance = numberScanner.nextDouble();
+        System.out.println("How much gas did you use? ");
+        // Get amount of gas used
+        double gasUsage = numberScanner.nextDouble();
+
+        // Calculate mpg using method metersToFeet
+        double milesPerGallon = metersToFeet(distance, gasUsage);
+
+        // Output calculations
+        System.out.println("Your used " + milesPerGallon + " miles to one gallon of gas");
+
+        numberScanner.close();
+        stringScanner.close();
+    }
+
+    private static double metersToFeet(double distanceMeters, double gasUsage) {
+        // Calculate the mpg
+        double mpg = (distanceMeters / gasUsage);
+        return mpg;
     }
 }
